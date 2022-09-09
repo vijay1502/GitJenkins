77ba66e8296d701187459b8660afdf3c703425db
vijay1502 <pruthvikrishna97@gmail.com>
2022-09-09 23:56:22 +0530

    again adding file

--- getAnotherSub.java
+++ getAnotherSub.java
@@ -2,5 +2,8 @@ public class getAnotherSub {
     public static void main(String[] args) {
         String first_string="MyOne";
         System.out.println(first_string.substring(0,3));
+        if(first_string.contains("O")){
+            System.out.println("Yes, this is good coding");
+        }
     }
 }
